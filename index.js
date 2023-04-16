2bf2a1913d2a7a133e87646596e15d04bc18dc72
andrew soulinhavong <soulinhavong@gmail.com>
2023-04-16 19:38:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainNode = document.querySelector('main#main');
+if (mainNode) {
+    mainNode.parentNode.removeChild(mainNode);
+  }
+
+const newHeader = document.createElement("h1");
+
+newHeader.id= 'victory';
+
+newHeader.innerHTML = "Andrew is the champion";
